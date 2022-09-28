311675b5753a31250fe22ad8d0f51cc2721fee64
rumgal <rumgal@gmail.com>
2022-09-28 18:47:17 +0200

    upd

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,4 +2,11 @@ var express = require("express");
 var router = express.Router();
 require("../models/connection");
 
+// TESTING
+router.get("/", (req, res) => {
+	const salutation = "Bienvenue a la gare";
+	console.log("let's go");
+	res.json({ Salutation: salutation });
+});
+
 module.exports = router;
